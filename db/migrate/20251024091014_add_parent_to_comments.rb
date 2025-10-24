d79c51d79364fbca23341a191034906399f627b0
Yolls24 <jruelle@hotmail.fr>
2025-10-24 11:37:24 +0200

    Add parent reference to comments

---
+++ db/migrate/20251024091014_add_parent_to_comments.rb
@@ -0,0 +1,11 @@
+class AddParentIdColumnToComments < ActiveRecord::Migration[8.0]
+  def change
+    # On ajoute la colonne manuellement pour éviter les contraintes NOT NULL
+    add_column :comments, :parent_id, :integer, null: true
+
+    # On ajoute l’index et la contrainte étrangère à la main
+    add_foreign_key :comments, :comments, column: :parent_id
+    add_index :comments, :parent_id
+  end
+end
+
