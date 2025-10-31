52f7ebe448ae5a0a8b494bca5f22f45ff570d71f
Martine9670 <pinnamartine@free.fr>
2025-10-31 09:49:38 +0100

    Translate two comments into English

--- db/migrate/20251024091014_add_parent_to_comments.rb
+++ db/migrate/20251024091014_add_parent_to_comments.rb
@@ -1,9 +1,9 @@
 class AddParentToComments < ActiveRecord::Migration[8.0]
   def change
-    # On ajoute la colonne manuellement pour éviter les contraintes NOT NULL
+    # add the column manually to avoid NOT NULL constraints.
     add_column :comments, :parent_id, :integer, null: true
 
-    # On ajoute l’index et la contrainte étrangère à la main
+    # add the index and the foreign key constraint manually.
     add_foreign_key :comments, :comments, column: :parent_id
     add_index :comments, :parent_id
   end
